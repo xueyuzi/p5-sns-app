fe5b0e3c8c8bdacb0d356da4bf3980696f288a52
鳕鱼子 <xueyu.d@gmail.com>
2018-12-17 23:02:07 +0800

    add inputText

--- lib/page/chart.dart
+++ lib/page/chart.dart
@@ -5,17 +5,45 @@ import "package:flutter_app/widget/dialog_clipper.dart";
 
 class ChartPage extends StatelessWidget {
   final List<Chart> chartList = [
-    Chart(avatar: "assets/images/cart_red.png", chartText: "很抱歉在这个时候打扰各位"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "好正经！"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "真有礼貌"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "但感觉不够亲近耶"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "但感觉不够亲近耶"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "但感觉不够亲近耶"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "但感觉不够亲近耶"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "但感觉不够亲近耶"),
-    Chart(avatar: "assets/images/cart_red.png", chartText: "但感觉不够亲近耶"),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "很抱歉在这个时候打扰各位",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "好正经！",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "真有礼貌",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "但感觉不够亲近耶",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "但感觉不够亲近耶",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "但感觉不够亲近耶",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "但感觉不够亲近耶",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "但感觉不够亲近耶",
+        chartDirection: Direction.Left),
+    Chart(
+        avatar: "assets/images/cart_red.png",
+        chartText: "但感觉不够亲近耶",
+        chartDirection: Direction.Right),
   ];
-  final double topHeight = 100;
+  final double topHeight = 100.0;
+  final double inputHeight = 45.0;
   Widget _buildAvatarList() {
     return Container(
       margin: EdgeInsets.fromLTRB(50, 70, 0, 0),
@@ -55,11 +83,16 @@ class ChartPage extends StatelessWidget {
     return Container(
         margin: EdgeInsets.symmetric(vertical: 30),
         child: Row(
+          mainAxisAlignment: chart.chartDirection == Direction.Left
+              ? MainAxisAlignment.start
+              : MainAxisAlignment.end,
           children: <Widget>[
-            Container(
-              width: 100,
-              child: Image.asset(chart.avatar),
-            ),
+            chart.chartDirection == Direction.Left
+                ? Container(
+                    width: 100,
+                    child: Image.asset(chart.avatar),
+                  )
+                : Text(""),
             _buildChartDialog(chart.chartText)
           ],
         ));
@@ -68,12 +101,38 @@ class ChartPage extends StatelessWidget {
   Widget _buildChartList() {
     return ListView(children: [
       Container(
-        margin: EdgeInsets.only(top: topHeight),
+        margin: EdgeInsets.only(top: topHeight, bottom: inputHeight),
         child: Column(
           children: chartList.map((chart) => _buildChartRow(chart)).toList(),
         ),
       )
     ]);
+  }
+
+  Widget _buildChartInput() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            height: inputHeight,
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black, width: 2)),
+            child: TextFormField(
+              enableInteractiveSelection: true,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.send, color: Colors.black),
+                  onPressed: () {debugPrint("send");},
+                ),
+              ),
+            ),
+          )
+        ]);
   }
 
   @override
@@ -83,13 +142,17 @@ class ChartPage extends StatelessWidget {
       children: <Widget>[
         _buildChartList(),
         _buildAvatarList(),
+        _buildChartInput()
       ],
     ));
   }
 }
 
+enum Direction { Right, Left }
+
 class Chart {
   String avatar;
   String chartText;
-  Chart({this.avatar, this.chartText});
+  Direction chartDirection;
+  Chart({this.avatar, this.chartText, this.chartDirection});
 }
